3db0ab61612c7fd5faee4c0b1d92184b8680391a
KHJ <kasperhelweg@gmail.com>
2016-10-27 20:10:30 +0200

    add initial meta.js for template

---
+++ meta.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "prompts": {
+        "name": {
+            "type": "string",
+            "required": true,
+            "message": "Project name"
+        },
+    },
+    "completeMessage": "Golden!"
+};
