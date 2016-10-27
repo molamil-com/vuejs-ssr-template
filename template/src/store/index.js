d98768793e08f11d393b301084a3b8432738fc47
KHJ <kasperhelweg@gmail.com>
2016-10-27 22:40:00 +0200

    cleanup store

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -6,20 +6,15 @@ Vue.use(Vuex)
 const defaultState = {
 }
 
-/* eslint-disable no-undef */
-/* eslint-disable no-underscore-dangle */
 const inBrowser = typeof window !== 'undefined'
 const state = (inBrowser && window.__INITIAL_STATE__) || defaultState
 
-// we could also just say s => ...
-/* eslint-disable no-shadow */
 const mutations = {
 }
 
 const actions = {
 }
 
-/* eslint-disable no-shadow */
 const getters = {
 }
 
