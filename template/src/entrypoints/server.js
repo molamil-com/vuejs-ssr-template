bba7d00a9d3b8e50a81c0b53c9935b6a6bd02780
KHJ <kasperhelweg@gmail.com>
2016-10-27 23:28:48 +0200

    begin #1

--- template/src/entrypoints/server.js
+++ template/src/entrypoints/server.js
@@ -3,8 +3,7 @@ import { vm, router, store } from 'app'
 export default (context) => {
     router.push(context.url)
 
-    /* eslint-disable array-callback-return */
-    /* eslint-disable consistent-return */
+    /* eslint-disable array-callback-return, consistent-return */
     return Promise.all(router.getMatchedComponents().map((component) => {
         if (component) {
             // Look into exactly how....
