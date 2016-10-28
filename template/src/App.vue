eb7b7576b4305a0ee6bdff859657cc4c1d6c813f
KHJ <kasperhelweg@gmail.com>
2016-10-28 16:06:21 +0200

    unrelated, clean main app

--- template/src/App.vue
+++ template/src/App.vue
@@ -21,29 +21,11 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
-}
-
-h1, h2 {
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
+    margin-top: 165px;
 }
 
 .logo {
     width: 105px;
-    margin-right: 10px;
     display: inline-block;
     vertical-align: middle;
 }
