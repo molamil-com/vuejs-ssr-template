17527b7ceccae937cfac158350f0b84a5245654b
KHJ <kasperhelweg@gmail.com>
2016-10-31 20:38:14 +0100

    Add basic logging.
    This commit adds basic logging using winston. It does not change the
    status of #8 since it is considered premature.
    
    See also: #8

--- template/build/development/runServer.js
+++ template/build/development/runServer.js
@@ -16,19 +16,19 @@ const { output } = webpackConfig.node[1]
 const serverPath = path.join(output.path, output.filename)
 
 function runServer(fs, cb) {
-    function onStdOut(data) {
+    function onData(data) {
         const time = new Date().toTimeString()
-        const match = data.toString('utf8').match(SERVER_READY_MESSAGE)
+        const listening = data.toString('utf8').match(SERVER_READY_MESSAGE)
 
         logger.log('info', `${data}`)
 
-        if(match) {
-            server.stdout.removeListener('data', onStdOut)
+        if(listening) {
+            server.stdout.removeListener('data', onData)
             server.stdout.on('data', data => logger.log('info', `${data}`))
 
             if(cb) {
                 emitter = new events.EventEmitter()
-                cb(null, match[1], emitter)
+                cb(null, listening[1], emitter)
             } else {
                 if(fs.hot()) emitter.emit('hot')
             }
@@ -48,8 +48,8 @@ function runServer(fs, cb) {
         silent: false,
     })
 
-    server.stdout.on('data', onStdOut)
-    server.stderr.on('data', x => process.stderr.write(x))
+    server.stdout.on('data', onData)
+    server.stderr.on('data', data => logger.log('error', `${data}`))
 }
 
 process.on('exit', () => {
