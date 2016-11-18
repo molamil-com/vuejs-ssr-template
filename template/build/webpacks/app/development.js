bcfaef34924c6ac3e7edb2172622ad00bb32193f
khj <kh@molamil.com>
2016-11-18 17:37:03 +0100

    uglify dev builds with beautify

--- template/build/webpacks/app/development.js
+++ template/build/webpacks/app/development.js
@@ -45,5 +45,8 @@ export default {
             inject: true,
             chunksSortMode: 'dependency',
         }),
+        new webpack.optimize.UglifyJsPlugin({
+            beautify: true
+        }),
     ],
 }
