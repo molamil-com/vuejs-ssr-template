dfa87d4fc9e322aee4bea45cb41a4e56f5ad742a
khj <kh@molamil.com>
2016-11-23 14:46:34 +0100

    add debug namespace app

--- template/build/webpacks/base.js
+++ template/build/webpacks/base.js
@@ -1,3 +1,4 @@
+import webpack from 'webpack'
 import config from '../../config/config'
 
 const baseConfig = {
@@ -35,6 +36,9 @@ const baseConfig = {
         ],
     },
     plugins: [
+        new webpack.ProvidePlugin({
+            debug: 'debug',
+        }),
         // new webpack.NoErrorsPlugin(),
     ],
 }
