b6fadaa86f62175b3ece79385bee304946f25c00
khj <kh@molamil.com>
2016-12-05 18:12:45 +0100

    Add import strip loader to bundle webpack

--- template/build/webpacks/server/serverBundle.js
+++ template/build/webpacks/server/serverBundle.js
@@ -1,7 +1,23 @@
 import webpack from 'webpack'
 import nodeExt from 'webpack-node-externals'
+import combineLoaders from 'webpack-combine-loaders'
 
 import config from '../../../config/config'
+
+const re = `import.*(${config.modules.browser.map((item) => { return "\\'" + item + "\\'" }).join('|')})`
+const sLoader = combineLoaders([
+    {
+        loader: 'babel-loader',
+    },
+    {
+        loader: 'string-replace-loader',
+        query: {
+            search: re,
+            replace: '/* eslint-disable no-undef */',
+            flags: 'g',
+        }
+    }
+]);
 
 const serverBundleConfig = {
     entry: [`${config.path.src}/entrypoints/server.js`],
@@ -20,10 +36,6 @@ const serverBundleConfig = {
                 exclude: /node_modules/,
             },
             {
-                test: /vue-slick[\\\/].*\.vue$/,
-                loader: config.path.root + '/build/development/tools/blockLoader'
-            },
-            {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 loader: 'url-loader',
             },
@@ -35,9 +47,16 @@ const serverBundleConfig = {
     },
     target: 'node',
     externals: [nodeExt({
-        whitelist: config.modules.browser
+        whitelist: config.modules.server.white
     })],
     plugins: [
+        new webpack.LoaderOptionsPlugin({
+            vue: {
+                loaders: {
+                    js: sLoader
+                },
+            },
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
             'process.env.VUE_ENV': '"server"'
