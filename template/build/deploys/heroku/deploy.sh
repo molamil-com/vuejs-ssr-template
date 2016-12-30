022c436e258313610a5bf1aab65fa6cd1589d4f7
khj <kh@molamil.com>
2016-12-30 14:12:42 +0100

    heroku deploys

--- template/build/deploys/heroku/deploy.sh
+++ template/build/deploys/heroku/deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 export PYTHONIOENCODING=utf8
+PATH_TO_DIST=$1; HEROKU_APP=$2
 
-path_to_dist=$1
+slugs_endpoint=https://api.heroku.com/apps/$HEROKU_APP/slugs
+releases_endpoint=https://api.heroku.com/apps/$HEROKU_APP/releases
+
 cd "$(dirname "$0")/release"; mkdir app; cd app
 
 echo "preparing slug assets..."
-cp $path_to_dist/package.json .
-cp -r $path_to_dist/dist .
+cp $PATH_TO_DIST/package.json .
+cp -r $PATH_TO_DIST/dist .
 
 echo "preparing slug runtime"
 npm install --production
@@ -17,10 +20,10 @@ cd ..
 tar czfv slug.tgz "./app" > /dev/null 2>&1
 
 echo "deploying slug..."
-resp=$(curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/vnd.heroku+json; version=3' -H "Authorization: Bearer 9ae7a056-1aa4-429b-b42b-302db081fe2d" -d '{"process_types":{"web":"node-v6.7.0-linux-x64/bin/node dist/server.js"}}' 'https://api.heroku.com/apps/r24syv-frontend-staging-v2/slugs')
+resp=$(curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/vnd.heroku+json; version=3' -H "Authorization: Bearer ${DEPLOY_KEY}" -d '{"process_types":{"web":"node-v6.7.0-linux-x64/bin/node dist/server.js"}}' $slugs_endpoint)
 
-url=$(echo ${resp} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["blob"]["url"];')
-slug=$(echo ${resp} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"];')
+blob_url=$(echo ${resp} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["blob"]["url"];')
+slug_id=$(echo ${resp} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"];')
 
-curl -X PUT -H "Content-Type:" --data-binary @slug.tgz $url
-curl -X POST -H "Accept: application/vnd.heroku+json; version=3" -H "Content-Type: application/json" -H "Authorization: Bearer ${DEPLOY_KEY}" -d '{"slug":'\"${slug}\"'}' https://api.heroku.com/apps/r24syv-frontend-staging-v2/releases
+curl -X PUT -H "Content-Type:" --data-binary @slug.tgz $blob_url
+curl -X POST -H "Accept: application/vnd.heroku+json; version=3" -H "Content-Type: application/json" -H "Authorization: Bearer ${DEPLOY_KEY}" -d '{"slug":'\"${slug_id}\"'}' $releases_endpoint
