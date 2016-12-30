daed790d08e06d4794528d9525c6b1a6aa03dac9
khj <kh@molamil.com>
2016-12-30 01:11:31 +0100

    deploy

--- template/build/deploys/heroku/deploy.sh
+++ template/build/deploys/heroku/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PYTHONIOENCODING=utf8
 
 path_to_dist=$1
 cd "$(dirname "$0")/release"; mkdir app; cd app
@@ -13,7 +14,13 @@ curl http://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-x64.tar.gz | tar xzv > /dev
 
 echo "packaging slug..."
 cd ..
-tar czfv slug.tgz "$(PWD)/app" > /dev/null 2>&1
+tar czfv slug.tgz "./app" > /dev/null 2>&1
 
-ls -la
-ls -la ./app
+echo "deploying slug..."
+resp=$(curl -X POST -H 'Content-Type: application/json' -H 'Accept: application/vnd.heroku+json; version=3' -H "Authorization: Bearer 9ae7a056-1aa4-429b-b42b-302db081fe2d" -d '{"process_types":{"web":"node-v6.7.0-linux-x64/bin/node dist/server.js"}}' 'https://api.heroku.com/apps/r24syv-frontend-staging-v2/slugs')
+
+url=$(echo ${resp} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["blob"]["url"];')
+slug=$(echo ${resp} | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["id"];')
+
+curl -X PUT -H "Content-Type:" --data-binary @slug.tgz $url
+curl -X POST -H "Accept: application/vnd.heroku+json; version=3" -H "Content-Type: application/json" -H "Authorization: Bearer ${DEPLOY_KEY}" -d '{"slug":'\"${slug}\"'}' https://api.heroku.com/apps/r24syv-frontend-staging-v2/releases
