07095767b51f4fbe21e698c0ec6e86e47eeb90f8
khj <kh@molamil.com>
2017-01-02 08:00:57 +0100

    dont use server initial store state

--- template/src/entrypoints/client.js
+++ template/src/entrypoints/client.js
@@ -1,6 +1,12 @@
 import 'es6-promise/auto'
+{{#if_eq template 'basic'}}
+import { vm } from 'app'
+
+{{/if_eq}}
+{{#if_eq template 'ssr'}}
 import { vm, store } from 'app'
 
 /* eslint-disable no-underscore-dangle */
 store.replaceState(window.__INITIAL_STATE__)
+{{/if_eq}}
 vm.$mount('#app')
