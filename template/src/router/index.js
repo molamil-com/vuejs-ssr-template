77aed9ea399655ab6c54960c5810c5d4193293f8
Thomas Springborg <ts@Thomass-MacBook-Pro-2.local>
2017-05-18 17:51:12 +0200

    adding more routes for example setup

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,10 +1,44 @@
 import Router from 'vue-router'
 import Vue from 'vue'
-
+{{#unless_eq setup 'nothing'}}
+{{#unless_eq setup 'libs'}}
+import BasePage from 'components/pages/BasePage'
+import TypographyPage from 'components/pages/TypographyPage'
+import ComponentsPage from 'components/pages/ComponentsPage'
+import Footer from 'components/blocks/Footer'
+import Footer from 'components/blocks/MainMenu'
+{{/unless_eq}}
+{{/unless_eq}}
 Vue.use(Router)
 
+{{#unless_eq setup 'nothing'}}
+{{#unless_eq setup 'libs'}}
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        components: {
+            footer: Footer,
+            menu: MainMenu,
+            default: BasePage,
+        }
+    },
+    {
+        path: '/',
+        name: 'home',
+        components: {
+            footer: Footer,
+            menu: MainMenu,
+            default: BasePage,
+        }
+    },
+]
+{{/unless_eq}}
+{{/unless_eq}}
+{{#if_eq setup 'nothing'}}
 const routes = [
 ]
+{{/if_eq}}
 
 export default new Router({
     mode: 'history',
